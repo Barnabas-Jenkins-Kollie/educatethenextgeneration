d4e51c9f777fc271debea75ff5dafd925b6eb487
jenkins <jbko.0097@gmail.com>
2025-01-27 08:18:07 -0800

    ulljkjk

---
+++ add.js
@@ -0,0 +1,61 @@
+function add(Num1, num2) {
+    var firstnum;
+    var secondnum;
+    var sum = firstnum + secondnum
+    return sum    
+}
+
+add(4, 5)
+
+
+var names = ['james', "john", "peter"]
+
+for (const elements  in  names) {
+   console.log(elements[2]) 
+   
+}
+
+// var name = 'jason'
+if (var name = 'John') {
+    print "hello jason: welcome"
+} else {
+   print  "sorry! I don't know you"
+    
+}
+
+
+switch (departments) {
+    case 1:
+        console.log("Hello! this is the HR department ")
+        break;
+        case 2:
+        console.log("Hello! this is the MARKETING department")
+        break;
+        case 3:
+        console.log("Hello! this is the FASHION DESIGN department")
+        break;
+      
+
+    default: console.log("HR!")
+        break;
+}
+
+
+
+
+
+for (let day = 0; day <= 1; day++) {
+    const days = [
+        'sunday', 'monday', 'tuesday',
+        'wednesday', 'thursday', 'friday', 'saturday'
+    ];
+
+    console.log(days[1])
+    console.log(days[2])
+    console.log(days[3])
+    console.log(days[4])
+    console.log(days[5])
+    console.log(days[6])
+    console.log(days[7])
+    
+}
